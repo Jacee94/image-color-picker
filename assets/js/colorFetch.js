c8492f57a384bde8717d6a33f0942d338288b997
EfSoren <ef.sorensen@me.com>
2022-10-24 21:39:04 -0600

    cleaning

--- assets/js/colorFetch.js
+++ assets/js/colorFetch.js
@@ -12,24 +12,15 @@ var http = new XMLHttpRequest();
 http.onreadystatechange = function() {
 	if(http.readyState == 4 && http.status == 200) {
 		var palette = JSON.parse(http.responseText).result;
-        console.log(palette);
         for (var i = 0; i < 5; i++) {
             let currPalette = palette[i]
-            console.log(currPalette[0]);
             var finalScheme = `rgb(${currPalette[0]},${currPalette[1]},${currPalette[2]})`
-            console.log(finalScheme);
-            console.log(typeof finalScheme );
             let colorBox = $('<div>').attr(`id`,`theme${i}`).css(`background-color`, finalScheme)
-            console.log(colorBox);
             $('#theme-box').append(colorBox)
         }
-            
-        
-        }
-        
-	}
+    }
+}
 
-
-    http.open("POST", url, true);
-    http.send(JSON.stringify(data)); 
+http.open("POST", url, true);
+http.send(JSON.stringify(data)); 
 
