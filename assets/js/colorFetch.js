5f5616d0dcc07da7b0bed5606cd42632c2bf6e6b
EfSoren <ef.sorensen@me.com>
2022-10-25 21:03:19 -0600

    remove element bugfix

--- assets/js/colorFetch.js
+++ assets/js/colorFetch.js
@@ -1,32 +1,35 @@
-var colorOne = [44,44,44]
-var colorTwo = [255,255,255]
-var palette = []
+var colorOne = [44, 44, 44];
+var colorTwo = [255, 255, 255];
+var palette = [];
 var genUrl = "http://colormind.io/api/";
 var data = {
-	model : "default",
-	input : [colorOne,colorTwo,"N","N","N"]
-}
+  model: "default",
+  input: [colorOne, colorTwo, "N", "N", "N"],
+};
 
-function generateScheme(){
-    //colorOne = colorPickerOne 
-    //colorTwo = colorPickerTwo
-    
-var http = new XMLHttpRequest();
+function generateScheme() {
+  //colorOne = colorPickerOne
+  //colorTwo = colorPickerTwo
+  $("#theme-box").empty();
+  var http = new XMLHttpRequest();
 
-http.onreadystatechange = function() {
-	if(http.readyState == 4 && http.status == 200) {
-		var palette = JSON.parse(http.responseText).result;
-        for (var i = 0; i < 5; i++) {
-            let currPalette = palette[i]
-            var finalScheme = `rgb(${currPalette[0]},${currPalette[1]},${currPalette[2]})`
-            let colorBox = $('<div>').attr(`id`,`theme${i}`).css(`background-color`, finalScheme).text(finalScheme)
-            $('#theme-box').append(colorBox)
-        }
+  http.onreadystatechange = function () {
+    if (http.readyState == 4 && http.status == 200) {
+      var palette = JSON.parse(http.responseText).result;
+      for (var i = 0; i < 5; i++) {
+        let currPalette = palette[i];
+        var finalScheme = `rgb(${currPalette[0]},${currPalette[1]},${currPalette[2]})`;
+        let colorBox = $("<div>")
+          .attr(`id`, `theme${i}`)
+          .css(`background-color`, finalScheme)
+          .text(finalScheme);
+        $("#theme-box").append(colorBox);
+      }
     }
+  };
+
+  http.open("POST", genUrl, true);
+  http.send(JSON.stringify(data));
 }
 
-http.open("POST", genUrl, true);
-http.send(JSON.stringify(data)); 
-}
-
-$('#theme-btn').on('click', generateScheme)
+$("#theme-btn").on("click", generateScheme);
