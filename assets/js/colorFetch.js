83f9f534a737c0f3f6f92a6ae72b0e8002583fea
EfSoren <ef.sorensen@me.com>
2022-10-27 20:00:47 -0600

    new api functionality

--- assets/js/colorFetch.js
+++ assets/js/colorFetch.js
@@ -1,34 +1,19 @@
-var colorOne = [44, 44, 44];
-var colorTwo = [255, 255, 255];
-var palette = [];
-var url = "http://colormind.io/api/";
 
-function generateScheme() {
-var dataE = {
-  model: "default",
-  input: [colorPickerOne, colorPickerTwo, "N", "N", "N"],
-};
-
+$("#theme-btn").on("click", buildBoxes);
+async function buildBoxes(){
+  const newColor = colorPickerOne.slice(1)
+  console.log(newColor);
+  let schemeUrl = `https://www.thecolorapi.com/scheme?hex=${newColor}`
+  console.log(schemeUrl);
   $("#theme-box").empty();
-  var http = new XMLHttpRequest();
-
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 200) {
-      var palette = JSON.parse(http.responseText).result;
-      for (var i = 0; i < 5; i++) {
-        let currPalette = palette[i];
-        var finalScheme = `rgb(${currPalette[0]},${currPalette[1]},${currPalette[2]})`;
-        let colorBox = $("<div>")
-          .attr(`id`, `theme${i}`)
-          .css(`background-color`, finalScheme)
-          .text(finalScheme);
-        $("#theme-box").append(colorBox);
-      }
-    }
-  };
-
-  http.open("POST", url, true);
-  http.send(JSON.stringify(dataE));
+  const result = await fetch(schemeUrl)
+  const data = await result.json()
+  const colorArray = data.colors
+  console.log(colorArray);
+  colorArray.forEach(element => {
+    let colorBox = $("<div>")
+      .css(`background-color`, element.hex.value)
+      .text(element.hex.value);
+    $("#theme-box").append(colorBox)
+  });
 }
-
-$("#theme-btn").on("click", generateScheme);
