d8a555aa0ed40093e93e1868a89ca2f449725898
jace.e94 <jace.edwards94@gmail.com>
2022-10-29 23:36:36 -0600

    local storage set and get functionality complete

--- assets/js/colorFetch.js
+++ assets/js/colorFetch.js
@@ -1,19 +1,56 @@
-
 $("#theme-btn").on("click", buildBoxes);
+
 async function buildBoxes(){
-  const newColor = colorPickerOne.slice(1)
-  console.log(newColor);
-  let schemeUrl = `https://www.thecolorapi.com/scheme?hex=${newColor}`
-  console.log(schemeUrl);
+  const newColor = colorPickerOne.slice(1);
+
+  let schemeUrl = `https://www.thecolorapi.com/scheme?hex=${newColor}`;
+
   $("#theme-box").empty();
-  const result = await fetch(schemeUrl)
-  const data = await result.json()
-  const colorArray = data.colors
-  console.log(colorArray);
+  const result = await fetch(schemeUrl);
+  const data = await result.json();
+  const colorArray = data.colors;
+
   colorArray.forEach(element => {
     let colorBox = $("<div>")
-      .css(`background-color`, element.hex.value)
-      .text(element.hex.value);
-    $("#theme-box").append(colorBox)
+                  .css(`background-color`, element.hex.value)
+                  .text(element.hex.value);
+    $("#theme-box").append(colorBox);
+  });
+
+  saveColorToStorage(colorPickerOne);
+}
+
+function saveColorToStorage(color) {
+  const colorHistory = JSON.parse(localStorage.getItem('color-picker-history')) || [];
+  const filteredColorHist = colorHistory.filter(item => item != color);
+  filteredColorHist.unshift(color);
+  if(filteredColorHist.length > 10) {
+    filteredColorHist.pop();
+  }
+  localStorage.setItem('color-picker-history', JSON.stringify(filteredColorHist));
+  addColorHistoryButtons();
+}
+
+function addColorHistoryButtons() {
+  const $colorHist = $('.color-history');
+  console.log($colorHist);
+  $colorHist.empty();
+  
+  const colorHist = JSON.parse(localStorage.getItem('color-picker-history'));
+  console.log(colorHist);
+  colorHist.forEach((color) => {
+    const newBtn = `<button class='color-history-btn m-2 button is-dark'>${color}</button>`
+
+    $colorHist.append(newBtn);
+  })
+
+  console.log($('.color-history-btn'))
+
+  $('.color-history-btn').on('click', (event) => {
+    event.preventDefault();
+    colorPickerOne = event.target.innerText;
+    buildBoxes();
   });
 }
+
+addColorHistoryButtons();
